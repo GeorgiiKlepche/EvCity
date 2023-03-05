aa586e33a885d0687d55be58e062f81b48b7e206
GeorgiiKlepche <98801688+GeorgiiKlepche@users.noreply.github.com>
2023-03-05 22:26:59 +0700

    Edit lab

---
+++ EventsCity/Views/Pages/ExcursView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EventsCity.Views.Pages
+{
+    public partial class ExcursView : UserControl
+    {
+        public ExcursView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EventsCity/Views/Pages/LifeView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace EventsCity.Views.Pages
+{
+    public partial class LifeView : UserControl
+    {
+        public LifeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
